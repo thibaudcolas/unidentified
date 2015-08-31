ce2d5ac5301a983b4cabcef8485214fc906e6b12
Thibaud Colas <thibaudcolas@gmail.com>
2015-09-01 00:25:02 +1200

    Minimal express server

--- server.js
+++ server.js
@@ -1,3 +1,18 @@
-const world = 'World';
+import express from 'express';
+import path from 'path';
+import logger from 'morgan';
+import bodyParser from 'body-parser';
 
-console.log(`Hello, ${world}!`);
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+const port = app.get('port');
+
+app.listen(port, () => {
+    console.log(`Express server listening on port ${port}`);
+});
